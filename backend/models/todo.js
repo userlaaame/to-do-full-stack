6e106fed6b201860aa0bcce2e23203cb8a8bbfd9
Brian Jackson Jr. <userlaaame@users.noreply.github.com>
2026-07-30 11:09:47 -0400

    added todo.js

---
+++ backend/models/todo.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const todoSchema = mongoose.Schema({
+    text: { type: String, required: true },
+    completed: { type: Boolean, default: false }
+})
+
+const Todo = mongoose.model('todo', todoSchema);
+
+export default Todo;
