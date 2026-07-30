04bfa1886bfa05ae2a2db6c9871f962a957ef8a2
Brian Jackson Jr. <userlaaame@users.noreply.github.com>
2026-07-30 13:22:09 -0400

    added post to backend

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,25 @@ const port = 3000;
 // allow requests from frontend
 app.use(cors());
 
+// format incoming data to json
+app.use(express.json());
+
+//get todos
 app.get('/api/todos', async (req, res) => {
     try {
         const todos = await Todo.find({});
         res.json(todos);
-    } catch(e) {
+    } catch (e) {
         console.error('Error fetching todos:', e);
         res.status(500).json({ error: e.message });
     }
+})
+
+// POST create a todo
+app.post('/api/todos', async (req, res) => {
+    console.log(req.body);
+    const todo = await Todo.create(req.body);
+    res.json(todos);
 })
 
 const startServer = async () => {
